27e5fae643593e63bd649dd70ee14b8c58d6a52f
Ubuntu <ubuntu@ip-172-31-42-67.ap-northeast-1.compute.internal>
2022-05-21 05:34:57 +0000

    finish task controller action

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_17_113108) do
+ActiveRecord::Schema.define(version: 2022_05_19_093841) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2022_05_17_113108) do
     t.boolean "all_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "schedule_memo"
   end
 
 end
